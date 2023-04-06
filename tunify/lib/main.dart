371b97e431c923c2459ff0019f5ec9f1e9ae9106
Asrat77 <112464138+Asrat77@users.noreply.github.com>
2023-04-06 22:22:50 +0300

    Auth Initial Commit

--- tunify/lib/main.dart
+++ tunify/lib/main.dart
@@ -2,9 +2,31 @@ import 'package:flutter/material.dart';
 import 'package:tunify/ui/screens/home_screen.dart';
 import 'package:tunify/ui/screens/login_screen.dart';
 import 'package:tunify/ui/screens/playlist_screen.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const MyApp());
+}
+
+class AuthWrapper extends StatelessWidget {
+  const AuthWrapper({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder<User?>(
+      stream: FirebaseAuth.instance.authStateChanges(),
+      builder: (context, snapshot) {
+        if (snapshot.hasData) {
+          return const Playlist();
+        } else {
+          return const Login_Screen();
+        }
+      },
+    );
+  }
 }
 
 class MyApp extends StatelessWidget {
@@ -13,9 +35,21 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(initialRoute: "/", routes: {
-      "/": (context) => Login_Screen(),
-      "/pl": (context) => Playlist(),
-    });
+    return MaterialApp(
+        home: StreamBuilder<User?>(
+          stream: FirebaseAuth.instance.authStateChanges(),
+          builder: (context, snapshot) {
+            if (snapshot.hasData) {
+              return const Playlist();
+            } else {
+              return const Login_Screen();
+            }
+          },
+        ),
+        //initialRoute: "/hom",
+        routes: {
+          "/home": (context) => Login_Screen(),
+          "/pl": (context) => Playlist(),
+        });
   }
 }
