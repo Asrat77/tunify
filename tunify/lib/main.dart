d188221a23a2994d7ed1691334b5fdc3a18d9ca7
Asrat77 <112464138+Asrat77@users.noreply.github.com>
2023-04-12 18:24:11 +0300

    Edited Navigation

--- tunify/lib/main.dart
+++ tunify/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:tunify/bloc/auth_bloc.dart';
 import 'package:tunify/services/auth.dart';
 import 'package:tunify/ui/screens/home_screen.dart';
 import 'package:tunify/ui/screens/login_screen.dart';
@@ -7,6 +8,9 @@ import 'package:tunify/ui/screens/playlist_screen.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:tunify/wrapper.dart';
+import 'package:bloc/bloc.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -14,47 +18,16 @@ void main() async {
   runApp(const MyApp());
 }
 
-// class AuthWrapper extends StatelessWidget {
-//   const AuthWrapper({Key? key}) : super(key: key);
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return StreamBuilder<User?>(
-//       stream: FirebaseAuth.instance.authStateChanges(),
-//       builder: (context, snapshot) {
-//         if (snapshot.hasData) {
-//           return const Playlist();
-//         } else {
-//           return const Login_Screen();
-//         }
-//       },
-//     );
-//   }
-// }
-
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MultiProvider(
-      providers: [
-        Provider<Auth>(
-          create: (_) => Auth(),
-        ),
-      ],
+    return BlocProvider(
+      create: (context) =>
+          AuthBloc(FirebaseAuth.instance),
       child: MaterialApp(
-          home: StreamBuilder<User?>(
-            stream: FirebaseAuth.instance.authStateChanges(),
-            builder: (context, snapshot) {
-              if (snapshot.hasData) {
-                return const Playlist();
-              } else {
-                return const Login_Screen();
-              }
-            },
-          ),
           initialRoute: "/home",
           routes: {
             "/home": (context) => const Login_Screen(),
