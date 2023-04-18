1f19afd69e0e991a3527ee006ebdc1b65fcf5425
dagi2002 <Dagemamogne@gmail.com>
2023-04-18 11:00:53 +0300

    update

--- tunify/lib/ui/screens/quiz_screen.dart
+++ tunify/lib/ui/screens/quiz_screen.dart
@@ -394,6 +394,13 @@ class _modeState extends State<mode> {
       theme: _isDarkMode ? darkTheme : lightTheme,
       home: Scaffold(
         appBar: AppBar(
+        leading: IconButton(
+                    icon: Icon(Icons.arrow_back),
+                    color: Colors.green,
+                    onPressed: () {
+                      Navigator.of(context).pop();
+          },
+        ),
           actions: [
             Switch(
               value: _isDarkMode,
@@ -403,15 +410,9 @@ class _modeState extends State<mode> {
                 });
               },
             )
+
           ],
 
-          //     Switch(
-          //     value: _isDarkMode,
-          //     onChanged: (value) {
-          //       setState(() {
-          //         _isDarkMode = value;
-          //       });
-          //     },
           // )
         ),
         body: Column(children: [
