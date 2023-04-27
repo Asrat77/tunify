9a478684c8fb75539cd05f50406d7e1529ef7f74
Asrat77 <112464138+Asrat77@users.noreply.github.com>
2023-04-27 18:12:16 +0300

    Create spotify_event.dart

---
+++ tunify/lib/bloc/spotifyBloc/spotify_event.dart
@@ -0,0 +1,24 @@
+part of 'spotify_bloc.dart';
+
+@immutable
+abstract class SpotifyEvent extends Equatable {
+  const SpotifyEvent();
+}
+
+class FetchEvent extends SpotifyEvent{
+  const FetchEvent();
+  @override
+  List<Object> get props => [];
+}
+
+
+class TrackEvent extends SpotifyEvent{
+  final Track track;
+
+  TrackEvent({required this.track});
+
+  @override
+  List<Object> get props => [];
+
+  get data => track;
+}
